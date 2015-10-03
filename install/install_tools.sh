c5a99187a5faf62beaca65049c52265ff465f51c
末永良太 <suenagaryoutaabc@gmail.com>
2015-10-03 12:45:41 +0900

    setup env to vagrant_centos

--- install/install_tools.sh
+++ install/install_tools.sh
@@ -8,3 +8,13 @@ sudo runuser -l vagrant -c 'gem install unicorn'
 echo "htop install..."
 sudo rpm -ivh http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
 sudo yum -y install htop
+
+echo "nodejs install..."
+sudo yum groupinstall 'Development tools'
+sudo runuser -l vagrant -c 'curl -O http://nodejs.org/dist/v0.10.32/node-v0.10.32.tar.gz'
+sudo runuser -l vagrant -c 'tar xvf node-v0.10.32.tar.gz'
+sudo runuser -l vagrant -c 'cd node-v0.10.32 ; ./configure --prefix=/usr/local'
+sudo runuser -l vagrant -c 'cd node-v0.10.32 ; make'
+sudo runuser -l vagrant -c 'cd node-v0.10.32 ; sudo make install'
+
+
